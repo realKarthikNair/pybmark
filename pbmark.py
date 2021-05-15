e4503e09f6bd8f6543d7c3c37db5f06ab2cc47a2
Karthik Nair <78267371+realKarthikNair@users.noreply.github.com>
2021-05-15 12:52:56 +0530

    Update pbmark.py

--- pbmark.py
+++ pbmark.py
@@ -1,20 +1,34 @@
-#simple python script to benchmark your cpu
+# Simple python script to benchmark your cpu
 
-import timeit
+import timeit # timeit measures execution time of given code snippet(s)
+
+"""
+so basically our benchmarking script tries to do 0|1|2|3|4|5|6|7|8..... upto
+99998 
+"""
 
 bmark_script='"|".join(str(i) for i in range(99999))'
+
+
 print("Running CPU Benchmark.... ")
 print("Please wait until the test ends...")
 
+"""
+The raw_score of the cpu is time taken by it to iterate the benchmarking script
+1000 times in a row
+"""
+
 raw_score=round(timeit.timeit(bmark_script, number=1000), 0)
-if raw_score>201:raw_score=201
+
+if raw_score>300:raw_score=300
 elif raw_score<1:raw_score=1
 
 scoring_criteria={}
-scores=[i for i in range (1,201)];c=0
-for i in range(200,0,-1):
+scores=[i for i in range (1,301)];c=0
+for i in range(300,0,-1):
     scoring_criteria[scores[c]]=i; c+=1
 
-print(scoring_criteria)
+
 pc_score=scoring_criteria[raw_score]
-print(f"Your pc scored {pc_score/2} points out of 100 ! ")
+print(f"\nYour pc scored {round(pc_score/3,2)} points out of 100 ! ")
+
